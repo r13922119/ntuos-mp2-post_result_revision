462b0caa3293cd3946c690a6285bf38ea123500d
r13922119 <r13922119@ntu.edu.tw>
2025-03-31 17:37:58 +0800

    fourth.2 version (at my very best i spotted a small error)

--- kernel/slab.h
+++ kernel/slab.h
@@ -15,8 +15,8 @@ struct run {
 /**
  * struct slab - Represents a slab in the slab allocator.
  * @freelist_offset: Linked list of free objects.
- * @meta.@link: with link.next and link.prev pointing to "link" of the other slabs in the list; also, it is an empty list only when newly created
- * @meta.@num_objs_in_use: number of allocated objects.
+ * @num_objs_in_use: number of allocated objects.
+ * @link: with link.next and link.prev pointing to "link" of the other slabs in the list; also, it is an empty list only when newly created
  * 
  * when freelist_offset == 0, the slab is full; otherwise, it is partial or free
  * when num_objs_in_use == 0, the slab is free; otherwise, it is partial or full
